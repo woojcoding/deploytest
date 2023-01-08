ed92a54c36fb53e7428f7f9c8c2691fc4c732e52
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-01-08 16:31:28 +0900

    Initial commit

---
+++ src/main/java/com/woojcoding/deploytest/DeploytestApplication.java
@@ -0,0 +1,13 @@
+package com.woojcoding.deploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woojcoding/deploytest/DeploytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojcoding.deploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
